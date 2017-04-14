c12767e77569d8172e10089ee130853160afedd1
Thomas Keating <twkeating1@gmail.com>
2017-04-13 21:35:20 -0400

    first commit/skeleton

---
+++ script.js
@@ -0,0 +1,25 @@
+var div1 = "\
+	<div class='row'>\
+		<div class='col-md-12 header'>\
+			<h1> Cafe Keating </h1>\
+			<img src='http://parkresto.com/wp-content/themes/parkrestaurant/images/11onlinereservationpark.jpg'>\
+		</div>\
+	</div>";
+
+var div2 = "\
+	<div class='row'>\
+		<div class='col-md-8 tab'></div>\
+		<div class='col-md-4 sidebar'></div>\
+	</div>";
+
+
+$(document).ready( function() {
+		//$('#content').prepend(div1);
+		//$('#content').append(div2);
+		
+});
+	//("tab").on('click', function {
+	// switch tabs 
+	//
+	//});
+
